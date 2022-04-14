a82a6047ce3538f657408c129f8606be90187894
CoolCoderCarl <123qwekir2wl@gmail.com>
2022-04-14 16:15:41 +0300

    Update task2.groovy

--- vars/task2.groovy
+++ vars/task2.groovy
@@ -25,7 +25,7 @@ def call() {
         finally {
             echo "FINALLY !"
             echo "${currentBuild.currentResult}"
-//             echo "${ffj.FFJ_VARNAME}"
+            echo "${ffj.FFJ_VARNAME}"
 //             echo "${ffj.FFJ_VARNAME}"
 //             echo "${sfj.SFJ_VARNAME}"
         }
