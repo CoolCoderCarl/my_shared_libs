3340d0941babcbff34118ed2d29b8fb7bc2a5ba3
CoolCoderCarl <123qwekir2wl@gmail.com>
2022-04-14 11:22:18 +0300

    Update task3_java_version.groovy

--- vars/task3_java_version.groovy
+++ vars/task3_java_version.groovy
@@ -1,12 +1,14 @@
 def call() {
-    echo "${env.JAVA_HOME}"
-    sh "java -version"
-    
-    env.JAVA_HOME="${tool 'jdk_11'}/jdk-11/bin/"
-    echo "${env.JAVA_HOME}"
-    sh "${env.JAVA_HOME}/java -version"
-    
-    env.JAVA_HOME="${tool 'jdk_16'}/jdk-16/bin/"
-    echo "${env.JAVA_HOME}"
-    sh "${env.JAVA_HOME}/java -version"
+    node {
+        echo "${env.JAVA_HOME}"
+        sh "java -version"
+
+        env.JAVA_HOME="${tool 'jdk_11'}/jdk-11/bin/"
+        echo "${env.JAVA_HOME}"
+        sh "${env.JAVA_HOME}/java -version"
+
+        env.JAVA_HOME="${tool 'jdk_16'}/jdk-16/bin/"
+        echo "${env.JAVA_HOME}"
+        sh "${env.JAVA_HOME}/java -version"
+    }
 }
