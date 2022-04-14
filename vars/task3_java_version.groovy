2ac1e28bd27e28e5fa52447b63d1c269194973ef
CoolCoderCarl <123qwekir2wl@gmail.com>
2022-04-14 11:43:38 +0300

    Update task3_java_version.groovy

--- vars/task3_java_version.groovy
+++ vars/task3_java_version.groovy
@@ -1,4 +1,4 @@
-def call() {
+// def call() {
     node {
         
         stage ('JAVA Default'){
@@ -18,4 +18,4 @@ def call() {
             sh "${env.JAVA_HOME}/java -version"
         }
     }
-}
+// }
