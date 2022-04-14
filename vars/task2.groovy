23d96568efad7495b4d750e7d02bfc00b70b3d19
CoolCoderCarl <123qwekir2wl@gmail.com>
2022-04-14 13:29:22 +0300

    Create task2.groovy

---
+++ vars/task2.groovy
@@ -0,0 +1,25 @@
+node {
+
+    stage('Test stage') {
+        echo "${JOB_NAME}"
+    }
+  
+    stage('Build downstreams jobs') {
+      build 'FirstFreestyleJob'
+      build 'SecondFreestyleJob'
+    }
+  
+    stage('Build downstreams jobs') {
+      echo 'Env var 1'
+    }
+  
+    stage('Build downstreams jobs') {
+      echo 'Env var 2'
+    }
+
+    post {
+        always { 
+            echo 'POST'
+        }
+    }
+}
