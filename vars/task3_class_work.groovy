8b842434dbe1650d30891271d45c35eabad54330
CoolCoderCarl <123qwekir2wl@gmail.com>
2022-04-14 13:05:28 +0300

    Update task3_class_work.groovy

--- vars/task3_class_work.groovy
+++ vars/task3_class_work.groovy
@@ -1,29 +1,6 @@
-// def call(String text) {
+def call(String text) {
   
-//   def t = new Task3()
-//   t.printText(text)
+  def t = new Task3()
+  t.printText(text)
   
-  
-  
-// }
-
-
-def call() {
-    node {
-        
-        stage ('Stage One'){
-            String text = 'It is work !'
-            task3_class_work(text)
-        }
-        
-        stage ('Stage Two'){
-            String text = 'It is work !'
-            task3_class_work(text)
-        }
-        
-        stage ('Stage Three'){
-            String text = 'It is work !'
-            task3_class_work(text)
-        }
-    }
 }
